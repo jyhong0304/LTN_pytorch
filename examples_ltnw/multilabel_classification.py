282aa2d75fd3b0ed2c5f5efaf01585b36ee239ca
Jinyung Hong <jinyunghong@asu.edu>
2019-05-16 14:35:59 -0700

    New example of the wrapper of LTN

---
+++ examples_ltnw/multilabel_classification.py
@@ -0,0 +1,70 @@
+import logging
+logger = logging.getLogger()
+logger.basicConfig = logging.basicConfig(level=logging.DEBUG)
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+import logictensornetworks_wrapper as ltnw
+
+nr_samples=500
+
+data=np.random.uniform([0,0],[1.,1.],(nr_samples,2)).astype(np.float32)
+data_A=data[np.where(np.sum(np.square(data-[.5,.5]),axis=1)<.09)]
+data_B=data[np.where(np.sum(np.square(data-[.5,.5]),axis=1)>=.09)]
+
+ltnw.variable("?data",data)
+ltnw.variable("?data_A",data_A)
+ltnw.variable("?data_B",data_B)
+
+ltnw.predicate("A",2)
+ltnw.predicate("B",2)
+
+ltnw.axiom("forall ?data_A: A(?data_A)")
+ltnw.axiom("forall ?data_B: ~A(?data_B)")
+
+ltnw.axiom("forall ?data_B: B(?data_B)")
+ltnw.axiom("forall ?data_A: ~B(?data_A)")
+
+ltnw.axiom("forall ?data: A(?data) -> ~B(?data)")
+ltnw.axiom("forall ?data: B(?data) -> ~A(?data)")
+
+ltnw.initialize_knowledgebase(optimizer=torch.optim.RMSprop, initial_sat_level_threshold=.99)
+# The number of iterations were dramatically reduced.
+sat_level=ltnw.train(track_sat_levels=1000,sat_level_epsilon=.01,max_epochs=2000)
+
+result=ltnw.ask("A(?data)")
+plt.figure(figsize=(10,8))
+plt.subplot(2,2,1)
+plt.title("A(x) - training")
+plt.scatter(data[:,0],data[:,1],c=result.squeeze())
+plt.colorbar()
+
+plt.subplot(2,2,2)
+result=ltnw.ask("B(?data)")
+plt.title("B(x) - training")
+plt.scatter(data[:,0],data[:,1],c=result.squeeze())
+plt.colorbar()
+
+data_test=np.random.uniform([0,0],[1.,1.],(nr_samples,2)).astype(np.float32)
+ltnw.variable("?data_test",data_test)
+result=ltnw.ask("A(?data_test)")
+plt.subplot(2,2,3)
+plt.title("A(x) - test")
+plt.scatter(data_test[:,0],data_test[:,1],c=result.squeeze())
+plt.colorbar()
+
+result=ltnw.ask("B(?data_test)")
+plt.subplot(2,2,4)
+plt.title("B(x) -test")
+plt.scatter(data_test[:,0],data_test[:,1],c=result.squeeze())
+plt.colorbar()
+
+plt.show()
+
+ltnw.constant("a",[0.5,.5])
+ltnw.constant("b",[0.75,.75])
+print("a is in A: %s" % ltnw.ask("A(a)"))
+print("b is in A: %s" % ltnw.ask("A(b)"))
+print("a is in B: %s" % ltnw.ask("B(a)"))
+print("b is in B: %s" % ltnw.ask("B(b)"))
